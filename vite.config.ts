2be6ac0b6ae580461479cd4f96c46fe525fe69b4
Aymir Sunchugashev <as4n.tsu@gmail.com>
2025-03-26 13:24:05 +0700

    installed dependencies && configured project file

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,11 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias:{
+      styles:"/src/scss",
+      components:"/src/components",
+      hooks:"/src/hooks"
+    }
+  }
 })
